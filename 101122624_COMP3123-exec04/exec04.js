34cec65a503c7a34ca4ce218e3858d90eece6447
DistinctDynamo <nguyensteven578@gmail.com>
2025-09-24 16:11:32 -0400

    continuing with lab 4

--- 101122624_COMP3123-exec04/exec04.js
+++ 101122624_COMP3123-exec04/exec04.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 app.get('/hello',(req, res)=>{
     res.send('Hello Express JS!');
 });
 
+app.get("/user", (req, res) => {
+    const firstname = req.query.firstname || "Pritesh";
+    const lastname = req.query.lastname || "Patel";
+    res.json({
+        fname: firstname,
+        lname: lastname
+    })
+});
+
+app.post("/user/:firstname/:lastname",(req,res)=>{
+    const {firstname, lastname} = req.params
+    res.json({
+        fname: firstname,
+        lname: lastname
+    })
+});
+
+app.post("/users",(req,res)=>{
+    const array = [{firstname = "Pritesh", lastname = "Patel"},
+            {firstname = "john", lastname = "doe"},
+            {firstname = "jon", lastname= "rome"}
+        ] = req.body.users;
+
+    res.json(array);
+});
 
 
 app.listen(3000,()=>{
